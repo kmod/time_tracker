d849f88fffec2df745739abd7ce3091db532da60
Kevin Modzelewski <kevmod@gmail.com>
2016-12-02 00:17:55 -0800

    coming along

--- timetracker/tt/urls.py
+++ timetracker/tt/urls.py
@@ -3,5 +3,6 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
+    url(r'^enter$', views.enter, name='enter'),
     # url(r'^$', views.index, name='index'),
 ]
--- timetracker/tt/views.py
+++ timetracker/tt/views.py
@@ -1,3 +1,18 @@
-from django.shortcuts import render
+import time
+import urllib
+
+from django.shortcuts import render, get_object_or_404, redirect
+
+from .models import Log, LogDescription
 
 # Create your views here.
+
+# import django.utils.timezone
+# django.utils.timezone.activate(
+
+def enter(request):
+    context = dict(
+            start="12:00:00",
+            end=time.strftime("%H:%M:%S", time.localtime()),
+    )
+    return render(request, "tt/enter.html", context)
