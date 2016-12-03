9a2e6d4d78738e02c2bd977429eacf2e310ca790
Kevin Modzelewski <kevmod@gmail.com>
2016-12-02 18:14:03 -0800

    Very simple MVP

--- timetracker/tt/models.py
+++ timetracker/tt/models.py
@@ -6,6 +6,6 @@ class LogDescription(models.Model):
     title = models.CharField(max_length=255, unique=True)
 
 class Log(models.Model):
-    start = models.DateTimeField(db_index=True)
-    end = models.DateTimeField(db_index=True)
+    start = models.DateTimeField(db_index=True, null=False)
+    end = models.DateTimeField(db_index=True, null=False)
     description = models.ForeignKey(LogDescription)
--- timetracker/tt/views.py
+++ timetracker/tt/views.py
@@ -1,5 +1,5 @@
-import time
-import urllib
+import collections
+import datetime
 
 from django.db.models import Max
 from django.shortcuts import render, get_object_or_404, redirect
@@ -11,15 +11,62 @@ from .models import Log, LogDescription
 # import django.utils.timezone
 # django.utils.timezone.activate(
 
+FORMAT = "%Y-%m-%dT%H:%M"
+
 def enter(request):
     if request.method == "POST":
-        pass
+        assert request.POST["description"]
+        assert request.POST["start"]
+        assert request.POST["end"]
+
+        try:
+            desc = LogDescription.objects.get(title=request.POST['description'].strip())
+        except LogDescription.DoesNotExist:
+            desc = LogDescription(title=request.POST['description'].strip())
+        desc.save()
+
+        start = request.POST['start']
+        print(start)
+        start = datetime.datetime.strptime(request.POST['start'], FORMAT)
+        print(start)
+        end = request.POST['end']
+        print(end)
+        end = datetime.datetime.strptime(request.POST['end'], FORMAT)
+        print(end)
+        l = Log(description=desc, start=start, end=end)
+        l.save()
+        print(l.start)
+
+        return redirect("/enter")
+
     start = Log.objects.all().aggregate(Max('end'))['end__max']
     if start is None:
-        start = time.localtime()
-    print(start)
+        start = datetime.datetime.now()
+    else:
+        start = start.astimezone(None)
+    print(start, repr(start))
+
     context = dict(
-            start=time.strftime("%Y-%m-%dT%H:%M:%S", start),
-            end=time.strftime("%Y-%m-%dT%H:%M:%S", time.localtime()),
+            start=start.strftime(FORMAT),
+            end=datetime.datetime.now().strftime(FORMAT),
     )
     return render(request, "tt/enter.html", context)
+
+ProcessedLog = collections.namedtuple("ProcessedLog", ("start", "end", "duration", "description"))
+
+def list(request):
+    print(Log.objects.all()[0].start)
+
+    raw_logs = Log.objects.select_related("description").all().order_by('-end')[:10]
+    logs = []
+    for l in raw_logs:
+        logs.append(ProcessedLog(
+            l.start.astimezone(None).strftime("%-I:%M %p"),
+            l.end.astimezone(None).strftime("%-I:%M %p"),
+            l.end - l.start,
+            l.description.title))
+
+    context = dict(
+            logs=logs,
+    )
+    return render(request, "tt/list.html", context)
